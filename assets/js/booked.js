052155a146347f8c1a9f39f5a3abe82798d83d3b
Bronson <bt98199@gmail.com>
2019-03-14 21:27:18 -0700

    x

--- assets/js/booked.js
+++ assets/js/booked.js
@@ -6,45 +6,45 @@ $(document).ready(function() {
     // var person = $(this).attr("data-person");
     // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     //   person + "&api_key=dc6zaTOxFJmzC&limit=10";
-    var queryURL= "https://sea.jumpbikes.com/opendata/free_bike_status.json";
+    var queryURL= "https://data.kingcounty.gov/resource/j56h-zgnm.json";
 
     $.ajax({
       url: queryURL,
-      method: "GET"
+      method: "GET",
+      data: {
+          "$limit" : 500,
+          "$$app_token" : "espxXkpZGfyr4LmWNhIiqFRVS"
+        //   "$$app_token" : "hCKMUtUkHvoev-6qD98KdKJYovlET4c1ZZfR"
+      }
     })
-      .then(function(response) {
-        var results = response.data.bikes;
-        var datResults = response.last_updated;
-        
+      .then(function(data) {
+        var results = data;
         for (var i = 0; i < results.length; i++) {
 
-          // let lastUpdate = results[i].last_updated;
-          // let lastUpdate = datResults;
-          let bikeId = results[i].bike_id;
-          let bikeName = results[i].name;
-          let isReserved = results[i].is_reserved;
-          let isDisabled = results[i].ios_reserved;
-          let batteryLevel = results[i].jump_ebike_battery_level;
-          let vehicleType = results[i].jump_vehicle_type;
-          let bikeLat = parseInt(1000*results[i].lat) / 1000;
-          let bikeLong = parseInt(1000*results[i].lon) / 1000;
+          let bookId = results[i].book_of_arrest_number;
+          let bookDate = moment(results[i].booking_date_time).format("ll");
+          let bookFirst = results[i].first_name;
+          let bookLast = results[i].last_name;
+          let bookCharge = results[i].charge;
+          let bookReleaseDate = moment(results[i].release_date_time).format("ll");
+          let releaseReason = results[i].release_reason;
+        //   let rcwCharge = results[i].rcw_ordinance_number;
+
 
            // Create the new row
          var newRow = $("<tr>").append(
-          //  $("<td>").text(lastUpdate),
-           $("<td>").text(bikeId),
-           $("<td>").text(bikeName),
-           $("<td>").text(isReserved),
-           $("<td>").text(isDisabled),
-           $("<td>").text(batteryLevel),
-           $("<td>").text(vehicleType),
-           $("<td>").text(bikeLat),
-           $("<td>").text(bikeLong)
+           $("<td>").text(bookId),
+           $("<td>").text(bookDate),
+           $("<td>").text(bookFirst),
+           $("<td>").text(bookLast),
+           $("<td>").text(bookCharge),
+           $("<td>").text(bookReleaseDate),
+           $("<td>").text(releaseReason)
+        //    $("<td>").text(rcwCharge)
         );
 
-          $("#jump-bikes-view > tbody").append(newRow);
+          $("#jump-bikes-view > tbody").prepend(newRow);
         }
-          $("#card-h").append("  Last updated: " + moment(datResults*1000).format('LLL'));
       });
 
 
